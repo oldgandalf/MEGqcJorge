d758317292859d17429182d8d083ef3a2bf4afb5
Evgeniia-G <93841835+Evgeniia-G@users.noreply.github.com>
2022-08-03 12:27:46 +0200

    Moving PSD report in universal_html_report.py too

--- Functions/universal_html_report.py
+++ Functions/universal_html_report.py
@@ -3,6 +3,20 @@ def make_RMSE_html_report(sid: str, what_data: str, list_of_figure_paths: list):
 
     '''Create an html report with figures
     Example: https://towardsdatascience.com/automated-interactive-reports-with-plotly-and-python-88dbe3aae5
+
+    NEED TO TURN THIS INTO UNIVERSAL REPORT FOR ALL DATA TYPES AND DIFFERENT NUMBER OF FIGURES.
+    WHAT TO WRITE AS DESCRIPTION FOR FIGURES?
+
+    Args: 
+    sid (str): subject id number, like '1'.
+    what_data (str): 'stds' or 'peaks'
+    list_of_figure_paths (list): list of paths to html extracted figues. 
+        they will be plotted in report in the order they are in this list
+
+
+    Returns:
+    the report itsels save on the local machine as htms file (in derivatives folder -> reports)
+
     '''
 
     if what_data=='stds':
@@ -112,4 +126,62 @@ def make_peak_html_report(sid: str, what_data: str, list_of_figure_paths: list):
     </html>'''
 
     with open('../derivatives/sub-'+sid+'/megqc/reports/report_'+fig_tit+'.html', 'w', encoding = 'utf8') as f:
+        f.write(html_string)
+
+
+
+def make_PSD_report(sid: str, list_of_figure_paths: list):
+
+    sid='1'
+
+    with open(list_of_figure_paths[0], 'r') as f1m:
+        fig1m = f1m.read()
+
+    with open(list_of_figure_paths[1], 'r') as f1g:
+        fig1g = f1g.read()
+        
+    with open(list_of_figure_paths[2], 'r') as f2m:
+        fig2m = f2m.read()
+
+    with open(list_of_figure_paths[3], 'r') as f2g:
+        fig2g = f2g.read()
+        
+    html_string = '''
+    <!doctype html>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>MEG QC: Frequency spectrum Report</title>
+            <style>body{ margin:0 100;}</style>
+        </head>
+        
+        <body style="font-family: Arial">
+            <center>
+            <h1>MEG data quality analysis report</h1>
+            <br></br>
+            <!-- *** Section 1 *** --->
+            <h2>Frequency spectrum per channel</h2>
+            ''' + fig1m + '''
+            <p>graph description...</p>
+
+            <br></br>
+            ''' + fig1g + '''
+            <p>graph description...</p>
+            
+            <!-- *** Section 2 *** --->
+            <br></br>
+            <br></br>
+            <br></br>
+            <h2>Relative power of each band over all channels</h2>
+            ''' + fig2m + '''
+            <p>graph description...</p>
+            <br></br>
+            ''' + fig2g + '''
+            <p>graph description...</p>
+            </center>
+        
+        </body>
+    </html>'''
+
+    with open('../derivatives/sub-'+sid+'/megqc/reports/report_PSD.html', 'w', encoding = 'utf8') as f:
         f.write(html_string)
