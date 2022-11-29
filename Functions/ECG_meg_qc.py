c910321297c54a5a7735ce7798cc909310a568d9
Evgeniia-G <93841835+Evgeniia-G@users.noreply.github.com>
2022-11-29 20:26:48 +0100

    continue with ecg errors:
    average looks same with/without ecg channel for. all datasets. but only 5 sets can be tested. of them 3. dont give anything as average event.

--- Functions/ECG_meg_qc.py
+++ Functions/ECG_meg_qc.py
@@ -28,13 +28,18 @@ def ECG_meg_qc(ecg_params: dict, raw: mne.io.Raw, m_or_g_chosen: list):
     ecg_deriv = []
 
     for m_or_g  in m_or_g_chosen:
-        fig_ecg = ecg_epochs.plot_image(combine='mean', picks = m_or_g[0:-1])[0]
+        fig_ecg = ecg_epochs.plot_image(combine='mean', picks = m_or_g[0:-1])[0] #plot averageg over ecg epochs artifact
+        # [0] is to plot only 1 figure. the function by default is trying to plot both mags and grads, but here we want 
+        # to do them saparetely depending on what was chosen for analysis
         ecg_deriv += [QC_derivative(fig_ecg, 'mean_ECG_epoch_'+m_or_g, None, 'matplotlib')]
 
         #averaging the ECG epochs together:
         avg_ecg_epochs = ecg_epochs.average().apply_baseline((-0.5, -0.2))
         fig_ecg_sensors = avg_ecg_epochs.plot_joint(times=[-0.25, -0.025, 0, 0.025, 0.25], picks = m_or_g[0:-1])
+        #plot average artifact with topomap
         ecg_deriv += [QC_derivative(fig_ecg_sensors, 'ECG_field_pattern_sensors_'+m_or_g, None, 'matplotlib')]
+
+    # Need to output: channels contamminated with ecg artifacts. 
 
     return ecg_deriv, ecg_events_times
 
