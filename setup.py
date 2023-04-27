1eaa6115d9ed1edfefbfecde59e5dc09ae14a089
AaronReer <aaron.reer123@gmail.com>
2023-04-27 16:58:52 +0200

    more edits to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import setup
+import versioneer
 
 setup(
     name='meg_qc',
     version= versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     packages=['meg_qc'],
     url='https://github.com/ANCPLabOldenburg/MEG-QC-code',
     license='MIT',
