e567a79428239a884256628cc46f516653a60235
AaronReer <aaron.reer123@gmail.com>
2023-05-11 12:16:40 +0200

    update versioneer and change style in setup.cfg

--- meg_qc/_version.py
+++ meg_qc/_version.py
@@ -43,7 +43,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "PEP440-pre"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = ""
     cfg.versionfile_source = "meg_qc/_version.py"
