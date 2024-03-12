24aeb3a09603e1904a8c9bbb3629e1fc368c7d99
Evgeniia Gapontseva <93841835+Evgeniia-Gapontseva@users.noreply.github.com>
2024-03-12 17:20:41 +0100

    selector with prompt_toolkit

--- meg_qc/inq_try.py
+++ meg_qc/inq_try.py
@@ -1,29 +1,3 @@
-# from prompt_toolkit.shortcuts import checkboxlist_dialog
-# from prompt_toolkit.styles import Style
-
-# results = checkboxlist_dialog(
-#     title="CheckboxList dialog",
-#     text="What would you like in your breakfast ?",
-#     values=[
-#         ("eggs", "Eggs"),
-#         ("bacon", "Bacon"),
-#         ("croissants", "20 Croissants"),
-#         ("daily", "The breakfast of the day")
-#     ],
-#     style=Style.from_dict({
-#         'dialog': 'bg:#cdbbb3',
-#         'button': 'bg:#bf99a4',
-#         'checkbox': '#e8612c',
-#         'dialog.body': 'bg:#a9cfd0',
-#         'dialog shadow': 'bg:#c98982',
-#         'frame.label': '#fcaca3',
-#         'dialog.body label': '#fd8bb6',
-#     })
-# ).run()
-
-# print(results)
-
-
 # import questionary
 
 # # Define the categories and subcategories
@@ -48,22 +22,23 @@
 # print('You selected:', subcategory)
 
 
+
 from prompt_toolkit.shortcuts import checkboxlist_dialog
 from prompt_toolkit.styles import Style
 
 # Define the categories and subcategories
 categories = {
-    'sub': {'029', '026', '025', '021', '016', '028', '022', '030', '018', '020', '009', '015', '013', '027', '014', '017', '012', '019', '023', '024', '031'},
-    'ses': {'1'},
-    'task': {'induction', 'deduction'},
-    'run': {1},
-    'desc': {'REPORT', 'ECGs', 'EOGs', 'Muscle', 'epochs', 'Sensors', 'PSDs', 'STDs', 'SimpleMetrics'}
+    'METRIC': ['_ALL_metrics_', 'ECGs', 'EOGs', 'Muscle', 'PSDs', 'STDs'],
+    'SUBJECT': ['_ALL_subjects_', '029', '026', '025', '021', '016', '028', '022', '030', '018', '020', '009', '015', '013', '027', '014', '017', '012', '019', '023', '024', '031'],
+    'SESSION': ['_ALL_sessions_', '1'],
+    'TASK': ['_ALL_tasks_', 'induction', 'deduction'],
+    'RUN': ['_ALL_runs_', 1]
 }
 
 # Create a list of values with category titles
 values = []
 for category, items in categories.items():
-    values.append(('', f'== {category} =='))
+    values.append((f'== {category} ==', f'== {category} =='))
     for item in items:
         values.append((str(item), str(item)))
 
@@ -82,4 +57,7 @@ results = checkboxlist_dialog(
     })
 ).run()
 
-print('You selected:', results)
+# Ignore the category titles
+selected_subcategories = [result for result in results if not result.startswith('== ')]
+
+print('You selected:', selected_subcategories)
