23a1ba1af099f491014927ece77f40395a632806
Evgeniia Gapontseva <93841835+Evgeniia-Gapontseva@users.noreply.github.com>
2024-03-12 17:08:47 +0100

    prompt toolkit selector

--- meg_qc/inq_try.py
+++ meg_qc/inq_try.py
@@ -1,15 +1,3 @@
-# import inquirer
-# questions = [
-#   inquirer.Checkbox('interests',
-#                     message="What are you interested in?",
-#                     choices=['Computers', 'Books', 'Science', 'Nature', 'Fantasy', 'History'],
-#                     ),
-# ]
-# answers = inquirer.prompt(questions)
-
-# print(answers["interests"])
-
-
 # from prompt_toolkit.shortcuts import checkboxlist_dialog
 # from prompt_toolkit.styles import Style
 
@@ -36,7 +24,32 @@
 # print(results)
 
 
-import questionary
+# import questionary
+
+# # Define the categories and subcategories
+# categories = {
+#     'sub': {'029', '026', '025', '021', '016', '028', '022', '030', '018', '020', '009', '015', '013', '027', '014', '017', '012', '019', '023', '024', '031'},
+#     'ses': {'1'},
+#     'task': {'induction', 'deduction'},
+#     'run': {1},
+#     'desc': {'REPORT', 'ECGs', 'EOGs', 'Muscle', 'epochs', 'Sensors', 'PSDs', 'STDs', 'SimpleMetrics'}
+# }
+
+# # Create a list of subcategories with category titles
+# subcategories = []
+# for category, items in categories.items():
+#     subcategories.append(questionary.Separator(f'== {category} =='))
+#     for item in items:
+#         subcategories.append(str(item))
+
+# # Ask the user to select a subcategory
+# subcategory = questionary.select("Select a subcategory:", choices=subcategories).ask()
+
+# print('You selected:', subcategory)
+
+
+from prompt_toolkit.shortcuts import checkboxlist_dialog
+from prompt_toolkit.styles import Style
 
 # Define the categories and subcategories
 categories = {
@@ -47,14 +60,26 @@ categories = {
     'desc': {'REPORT', 'ECGs', 'EOGs', 'Muscle', 'epochs', 'Sensors', 'PSDs', 'STDs', 'SimpleMetrics'}
 }
 
-# Create a list of subcategories with category titles
-subcategories = []
+# Create a list of values with category titles
+values = []
 for category, items in categories.items():
-    subcategories.append(questionary.Separator(f'== {category} =='))
+    values.append(('', f'== {category} =='))
     for item in items:
-        subcategories.append(str(item))
+        values.append((str(item), str(item)))
 
-# Ask the user to select a subcategory
-subcategory = questionary.select("Select a subcategory:", choices=subcategories).ask()
+results = checkboxlist_dialog(
+    title="CheckboxList dialog",
+    text="Select subcategories:",
+    values=values,
+    style=Style.from_dict({
+        'dialog': 'bg:#cdbbb3',
+        'button': 'bg:#bf99a4',
+        'checkbox': '#e8612c',
+        'dialog.body': 'bg:#a9cfd0',
+        'dialog shadow': 'bg:#c98982',
+        'frame.label': '#fcaca3',
+        'dialog.body label': '#fd8bb6',
+    })
+).run()
 
-print('You selected:', subcategory)
+print('You selected:', results)
