731a479820ca6d52861c295343051e35a7606b42
AaronReer <48220826+AaronReer@users.noreply.github.com>
2024-03-13 16:12:47 +0100

    edited setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='meg_qc',
     version= versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    packages=['meg_qc'],
+    packages=['meg_qc','meg_qc/source'],
     url='https://github.com/AaronReer/MEGqc',
     license='MIT',
     author='ANCP',
