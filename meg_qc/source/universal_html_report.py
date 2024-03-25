d1e67f77bcf17fc5ab0e9de9f7a439e0ba95d3c6
Evgeniia Gapontseva <93841835+Evgeniia-Gapontseva@users.noreply.github.com>
2024-03-25 13:15:18 +0100

    To avaoid import issues just added the func here from universal_plots into html_plots

--- meg_qc/source/universal_html_report.py
+++ meg_qc/source/universal_html_report.py
@@ -1,8 +1,57 @@
 
 import mne
 
-from source.universal_plots import get_tit_and_unit #for terminal
+#from source.universal_plots import get_tit_and_unit #for terminal
 #from universal_plots import get_tit_and_unit #for notebook
+
+#To avaoid import issues just added the func here from universal_plots:
+#TODO: remover when import issues arer fixed.
+
+def get_tit_and_unit(m_or_g: str, psd: bool = False):
+
+    """
+    Return title and unit for a given type of data (magnetometers or gradiometers) and type of plot (psd or not)
+    
+    Parameters
+    ----------
+    m_or_g : str
+        'mag' or 'grad'
+    psd : bool, optional
+        True if psd plot, False if not, by default False
+
+    Returns
+    -------
+    m_or_g_tit : str
+        'Magnetometers' or 'Gradiometers'
+    unit : str
+        'T' or 'T/m' or 'T/Hz' or 'T/m / Hz'
+
+    """
+    
+    if m_or_g=='mag':
+        m_or_g_tit='Magnetometers'
+        if psd is False:
+            unit='Tesla'
+        elif psd is True:
+            unit='Tesla/Hz'
+    elif m_or_g=='grad':
+        m_or_g_tit='Gradiometers'
+        if psd is False:
+            unit='Tesla/m'
+        elif psd is True:
+            unit='Tesla/m / Hz'
+    elif m_or_g == 'ECG':
+        m_or_g_tit = 'ECG channel'
+        unit = 'V'
+    elif m_or_g == 'EOG':
+        m_or_g_tit = 'EOG channel'
+        unit = 'V'
+    else:
+        m_or_g_tit = '?'
+        unit='?'
+
+    return m_or_g_tit, unit
+
 
 def make_html_section(derivs_section: list, section_name: str, report_strings: dict):
 
