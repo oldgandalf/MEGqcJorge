0fb97494eeda7ce206f549e62d325193a42e8cea
AaronReer <aaron.reer123@gmail.com>
2024-05-15 15:14:10 +0200

    small edits

--- docker/run_megqc.py
+++ docker/run_megqc.py
@@ -6,5 +6,5 @@ config_file_path = '/home/areer1/Projects/MEGqc_myfork/MEGqc/meg_qc/settings/set
 internal_config_file_path='/home/areer1/Projects/MEGqc_myfork/MEGqc/meg_qc/settings/settings_internal.ini' # internal settings in in
 #raw, raw_cropped_filtered_resampled, QC_derivs, QC_simple, df_head_pos, head_pos, scores_muscle_all1, scores_muscle_all2, scores_muscle_all3, raw1, raw2, raw3, avg_ecg, avg_eog = make_derivative_meg_qc(config_file_path, internal_config_file_path)
 
-for_report = make_derivative_meg_qc(config_file_path, internal_config_file_path)
+make_derivative_meg_qc(config_file_path, internal_config_file_path)
 
