c0e00680abfe265b9e81bc6a35dc1800358fd9de
AaronReer <aaron.reer123@gmail.com>
2024-11-26 14:24:36 +0100

    test entry points

--- meg_qc/run_megqc.py
+++ meg_qc/run_megqc.py
@@ -5,7 +5,8 @@ import argparse
 
 #dataset_path_parser = argparse.ArgumentParser(description= "parser for BIDS dataset path")
 #dataset_path_parser.add_argument("inputdata", type=str)
-
+def hello_world():
+    print("Hello World")
 #args = dataset_path_parser.parse_args()
 # Get the absolute path of the parent directory of the current script
 parent_dir = os.path.dirname(os.getcwd())
