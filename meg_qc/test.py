9004f3e2180b96b310876970e4f16be3bf1a85a6
AaronReer <aaron.reer123@gmail.com>
2024-11-26 17:40:40 +0100

    try to run the full process from commandline tool

--- meg_qc/test.py
+++ meg_qc/test.py
@@ -15,8 +15,16 @@ def run_megqc():
     dataset_path_parser.add_argument("--inputdata", type=str, required=True, help="path to the root of your BIDS MEG dataset")
     args=dataset_path_parser.parse_args()
 
+    path_to_entrypoint= os.path.abspath(__file__)
+
     parent_dir = os.path.dirname(os.getcwd())
     print(parent_dir)
+    print(path_to_entrypoint)
 
     data_directory = args.inputdata
     print(data_directory)
+
+    config_file_path = '/home/areer1/Projects/MEGqc_myfork/MEGqc/meg_qc/settings/settings.ini' 
+    internal_config_file_path='/home/areer1/Projects/MEGqc_myfork/MEGqc/meg_qc/settings/settings_internal.ini'
+
+    make_derivative_meg_qc(config_file_path, internal_config_file_path, data_directory)
