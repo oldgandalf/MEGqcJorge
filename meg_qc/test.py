fad837fcf6064b7a3496bfe1db3dfd4c01927d1f
AaronReer <aaron.reer123@gmail.com>
2024-11-27 14:39:15 +0100

    changed to relative paths for config file

--- meg_qc/test.py
+++ meg_qc/test.py
@@ -15,16 +15,16 @@ def run_megqc():
     dataset_path_parser.add_argument("--inputdata", type=str, required=True, help="path to the root of your BIDS MEG dataset")
     args=dataset_path_parser.parse_args()
 
-    path_to_entrypoint= os.path.abspath(__file__)
+    path_to_megqc_installation= os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir))
 
-    parent_dir = os.path.dirname(os.getcwd())
-    print(parent_dir)
-    print(path_to_entrypoint)
+    #parent_dir = os.path.dirname(os.getcwd())
+    #print(parent_dir)
+    print(path_to_megqc_installation)
 
     data_directory = args.inputdata
     print(data_directory)
 
-    config_file_path = '/home/areer1/Projects/MEGqc_myfork/MEGqc/meg_qc/settings/settings.ini' 
-    internal_config_file_path='/home/areer1/Projects/MEGqc_myfork/MEGqc/meg_qc/settings/settings_internal.ini'
+    config_file_path = path_to_megqc_installation + '/settings/settings.ini' 
+    internal_config_file_path=path_to_megqc_installation + '/settings/settings_internal.ini'
 
     make_derivative_meg_qc(config_file_path, internal_config_file_path, data_directory)
