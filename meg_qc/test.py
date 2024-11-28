522d922ef888ea75c066ce25a1c5d463540f9355
AaronReer <48220826+AaronReer@users.noreply.github.com>
2024-11-28 13:36:36 +0100

    adeed more cli functionality

--- meg_qc/test.py
+++ meg_qc/test.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import sys
 from meg_qc.calculation.meg_qc_pipeline import make_derivative_meg_qc
+from meg_qc.plotting.meg_qc_plots import make_plots_meg_qc
 
 def hello_world():
     dataset_path_parser = argparse.ArgumentParser(description= "parser for string to print")
@@ -35,8 +36,25 @@ def run_megqc():
 
     make_derivative_meg_qc(config_file_path, internal_config_file_path, data_directory)
 
+    user_input = input('Do you want to run the MEGqc plotting module on the MEGqc results? (y/n): ').lower().strip() == 'y'
+
+    if user_input == True:
+        make_plots_meg_qc(data_directory)
+    else:
+        print('MEGqc results can be found in' + data_directory +'/derivatives/MEGqc/calculation')
+
+
 def get_config():
+    import shutil
 
     path_to_megqc_installation= os.path.abspath(os.path.join(os.path.abspath(__file__), os.pardir))
+    config_file_path =path_to_megqc_installation +'/settings/settings.ini'
+    
+    shutil.copy(config_file_path, destination_directory)
+
+
+
+def get_plots():
+
 
 
