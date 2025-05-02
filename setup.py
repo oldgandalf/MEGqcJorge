ce385f6698feb88d8c071500e2295542dfd46a21
karellopez <karellopez@hotmail.es>
2025-05-02 17:40:54 +0200

    GUI implementation

--- setup.py
+++ setup.py
@@ -21,10 +21,19 @@ if __name__ == '__main__':
         cmdclass=versioneer.get_cmdclass(),
         setup_requires=SETUP_REQUIRES,
         install_requires=INSTALL_REQUIRES,
-        packages=['meg_qc','meg_qc/calculation','meg_qc/calculation/metrics','meg_qc/plotting','meg_qc/settings',
-                  'meg_qc/miscellaneous','meg_qc/miscellaneous/examples','meg_qc/miscellaneous/optimizations',
-                  'meg_qc/miscellaneous/GUI'
-                  ],
+          packages=[
+              'meg_qc',
+              'meg_qc.calculation',
+              'meg_qc.calculation.metrics',
+              'meg_qc.plotting',
+              'meg_qc.settings',
+              'meg_qc.miscellaneous',
+              'meg_qc.miscellaneous.examples',
+              'meg_qc.miscellaneous.optimizations',
+              'meg_qc.miscellaneous.GUI',
+          ],
+          include_package_data=True,
+          package_data={'meg_qc.miscellaneous.GUI': ['logo.png']},
         url='https://github.com/karellopez/MEGqc',
         entry_points={
             'console_scripts':[
